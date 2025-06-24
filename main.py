6cfa9f09260f6a11965c77b5272cf7f567a874ad
lujain415 <lujainalaamri55@gmail.com>
2025-06-24 21:32:45 +0300

    completed

---
+++ main.py
@@ -0,0 +1,14 @@
+class Pandas:
+    def __init__(self,name,age,color,gender):
+        self.name=name
+        self.age=age
+        self.color=color
+        self.gender=gender
+    def bears (self):
+        intrduce=(f"name: {self.name} age : {self.age} color : {self.color} gender : {self.gender}")
+        return intrduce
+    def sleep(self):
+        sleeping=(f"{self.name} sleep 22 hours ")
+        return sleeping
+
+
---
+++ object.py
@@ -0,0 +1,17 @@
+from main import Pandas
+
+bear1=Pandas("shehab",4,"brown","male")
+bear2=Pandas("qutbai",5,"white","male")
+bear3=Pandas("panda",6,"black","female")
+bear4=Pandas("loopw",7,"pink","female")
+print(bear1.bears())
+print(bear2.bears())
+print(bear3.bears())
+print(bear4.bears())
+
+print(bear1.name)
+
+print(bear1.sleep())
+print(bear2.sleep())
+print(bear3.sleep())
+print(bear4.sleep())
