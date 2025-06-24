3226c5406a440d0e813c339026a09ff747848eae
Moh0py <1vipmoh@gmail.com>
2025-06-24 17:00:15 +0300

    solve tasks

---
+++ panda.py
@@ -0,0 +1,16 @@
+class Panda:
+    def __init__(self, name, age, gender,color):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.color = color
+
+    def eat(self):
+            print(f"{self.name} is eating 🐼🥬 ")
+    def sleep(self):
+            print(f"{self.name} is sleeping 💤🐼 ")
+    def play(self):
+            print(f"{self.name} is playing 🐼🦵 ")
+        
+    
+                
